0ce7b602dddb14a070d91579be1a0faa7f6b00ca
Valzionn <144949469+Valzionn@users.noreply.github.com>
2024-05-21 15:30:03 +0000

    a

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 //installa react-router-dom
-import React from "react";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import HomePage from "./HomePage";
 import MenuPage from "./MenuPage";
