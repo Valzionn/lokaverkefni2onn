2a4916455c34da532e0ae3516642e0c1a6870427
Valzionn <144949469+Valzionn@users.noreply.github.com>
2024-06-07 20:41:59 +0000

    loop

--- app/SwiperComponent.tsx
+++ app/SwiperComponent.tsx
@@ -1,91 +1,56 @@
-import React, { useRef, useEffect, MutableRefObject } from 'react';
-import { register } from 'swiper/element/bundle';
-import 'swiper/swiper-bundle.css';
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import { EffectCoverflow, Pagination } from 'swiper/modules';
 import Image from 'next/image';
-import cat from './Cat.jpg';
+import cat1 from './Cat1.jpeg'; 
+import cat2 from './Cat2.png'; 
+import cat3 from './Cat3.png'; 
+import cat4 from './Cat4.png'; 
+import cat5 from './Cat5.png'; 
 
-register();
-
-declare global {
-    namespace JSX {
-        interface IntrinsicElements {
-            'swiper-container': any;
-            'swiper-slide': any;
-        }
-    }
+export default function SwiperComponent() {
+  return (
+    <div className="swiper-background border border-sky-200">
+      <Swiper
+        effect="coverflow"
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView="auto"
+        spaceBetween={30}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        loop={true}
+        pagination={{ clickable: true }}
+        modules={[EffectCoverflow, Pagination]}
+        className="h-[50vh] sm:h-[40vh] md:h-[30vh] lg:h-[25vh] xl:h-[20vh] w-fit"
+      >
+        {[cat1, cat2, cat3, cat4, cat5].map((image, index) => (
+          <SwiperSlide
+            key={index}
+            className="flex items-center justify-center p-0 m-0"
+            style={{ width: 'auto' }}
+          >
+            <div className="relative flex items-center justify-center w-full h-full p-0 m-0">
+              <Image
+                src={image}
+                alt={`Cat ${index + 1}`}
+                layout="responsive"
+                width={300}
+                height={300}
+                className="object-contain"
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 }
-
-const SwiperComponent = () => {
-    const swiperElRef = useRef<HTMLElement>(null);
-
-    useEffect(() => {
-        const swiperEl = swiperElRef.current;
-        if (swiperEl) {
-            const handleProgress = (e: any) => {
-                const [swiper, progress] = e.detail;
-                console.log(progress);
-            };
-
-            const handleSlideChange = () => {
-                console.log('slide changed');
-            };
-
-            swiperEl.addEventListener('progress', handleProgress);
-            swiperEl.addEventListener('slidechange', handleSlideChange);
-
-            return () => {
-                swiperEl.removeEventListener('progress', handleProgress);
-                swiperEl.removeEventListener('slidechange', handleSlideChange);
-            };
-        }
-    }, []);
-
-    return (
-        <div className="w-full bg-white">
-            <swiper-container
-                ref={swiperElRef as MutableRefObject<any>}
-                className="swiper-container h-[50vh] sm:h-[40vh] md:h-[30vh] lg:h-[25vh] xl:h-[20vh] w-full"
-                slides-per-view="1"
-                loop="true"
-                navigation="true"
-                pagination="true"
-            >
-                <swiper-slide className="flex items-center justify-center">
-                    <div className="h-full w-full flex items-center justify-center">
-                        <Image
-                            src={cat}
-                            alt="Cat"
-                            layout="intrinsic"
-                            width={500}
-                            height={500}
-                            className="responsive-img object-contain max-h-full max-w-full"
-                        />
-                    </div>
-                </swiper-slide>
-                <swiper-slide className="flex items-center justify-center">
-                    <div className="h-full w-full flex items-center justify-center">
-                        <Image
-                            src={cat}
-                            alt="Cat"
-                            layout="intrinsic"
-                            width={500}
-                            height={500}
-                            className="responsive-img object-contain max-h-full max-w-full"
-                        />
-                    </div>
-                </swiper-slide>
-                <swiper-slide className="flex items-center justify-center">
-                    <div>Slide 3</div>
-                </swiper-slide>
-                <swiper-slide className="flex items-center justify-center">
-                    <div>Slide 4</div>
-                </swiper-slide>
-                <swiper-slide className="flex items-center justify-center">
-                    <div>Slide 5</div>
-                </swiper-slide>
-            </swiper-container>
-        </div>
-    );
-};
-
-export default SwiperComponent;
